5734edef5da159a32791a5c88a85865cc616789a
MukulDevPatel <patelmukuldev@gmail.com>
2023-03-21 16:45:54 +0530

    [Muku Dev] Add: This is snake and ladder problem

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeAndLadderPlay
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Snake and Ladder play");
+            SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+
+        }
+    }
+}
