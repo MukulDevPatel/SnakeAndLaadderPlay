72b6208497ed51172459041f68d8c6080a6e6793
MukulDevPatel <patelmukuldev@gmail.com>
2023-03-21 19:05:25 +0530

    [Mukul Dev] Add: Winning Position

--- Program.cs
+++ Program.cs
@@ -7,17 +7,9 @@ namespace SnakeAndLadderPlay
         static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Snake and Ladder play");
-            if (args.Length==1)
-            {
-                SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
-                snakeAndLadder.DieRoll();
-                snakeAndLadder.Game();
-            }
-            else
-            {
-                Console.WriteLine("No player for game");
-            }
-
+            SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+            snakeAndLadder.DieRoll();
+            snakeAndLadder.Game();
         }
     }
 }
--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -8,32 +8,47 @@ namespace SnakeAndLadderPlay
 {
     public class SnakeAndLadder
     {
-        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100;
         int playerPosition = 0;  //UC1
+        int count=0;
         
         //UC2
         Random random = new Random();
         public int DieRoll()
         {
             int die = random.Next(1, 7);
+            Console.WriteLine("Die value ---->" + die);
+            count++;
             return die;
         }
         //UC3
         public void Game()
         {
-            int option = random.Next(1, 3);
-            switch (option)
+            while (this.playerPosition < WINNING_POSITION)
             {
-                case NO_PLAY:
-                    break;
-                case LADDER:
-                    this.playerPosition += DieRoll();
-                    Console.WriteLine("Ladder - moved ahead" + DieRoll());
-                    break;
-                case SNAKE:
-                    this.playerPosition -= DieRoll();
-                    Console.WriteLine("Snake - move behind" + DieRoll());
-                    break;
+                int option = random.Next(1, 3);
+                switch (option)
+                {
+                    case NO_PLAY:
+                        break;
+                    case LADDER:
+                        this.playerPosition += DieRoll();
+                        Console.WriteLine("Ladder - moved ahead" + DieRoll());
+                        break;
+                    case SNAKE:
+                        this.playerPosition -= DieRoll();
+                        Console.WriteLine("Snake - move behind" + DieRoll());
+                        break;
+                }
+                if (this.playerPosition < 0)
+                {
+                    playerPosition = 0;
+                    Console.WriteLine("You went zero, Restart game from 0");
+                }
+                if (this.playerPosition ==  WINNING_POSITION)
+                {
+                    Console.WriteLine("Player position--->" + playerPosition);
+                }
             }
         }
     }
